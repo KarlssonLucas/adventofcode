45fa468347824d2756fa679166625a6d036c70e5
lucaskarlsson <Lucaskarlsson@live.se>
2020-12-04 16:43:19 +0100

    part1 day4

--- day4/part1.hs
+++ day4/part1.hs
@@ -3,8 +3,16 @@ import Data.List.Split
 main :: IO ()
 main = do
     input <- readFile "input"
-    let wd = [ (splitOn "\n" x) | x <- (splitOn "\n\n" input)]
-    putStrLn $ show $ wd 
+    let wd = [toPair (words x) | x <- (splitOn "\n\n" input)]
+    let ls = [ x | x <- wd, acceptedPassports x]
+    putStrLn $ show $ length ls
 
+toPair []     = []
+toPair (s:ss) = a ++ (toPair ss)
+                    where a = splitOn ":" s
 
-
+acceptedPassports :: [String] -> Bool
+acceptedPassports []     = False 
+acceptedPassports list 
+  | length [s | s <- ["ecl","pid","hcl","eyr","hgt","iyr","byr"], s `elem` list] >= 7 = True
+  | otherwise = False
