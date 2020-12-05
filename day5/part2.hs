96fad49dcc23e232041c4df645f424f524bb8ec9
kakan <lucaskarlsson@live.se>
2020-12-05 16:39:14 +0100

    day5

---
+++ day5/part2.hs
@@ -0,0 +1,24 @@
+main :: IO ()
+main = do
+    inputs <- readFile "input"
+    let ls = [ (toRowCol line) | line <- lines inputs]
+    let finish = [ (fst x) * 8 + (snd x) | x <- ls]
+    putStrLn $ show $ unique finish 100
+
+unique :: [Int] -> Int -> Int
+unique [] n     = n
+unique list n | n `elem` list = unique list (n+1)
+              | otherwise     = n
+
+
+toRowCol :: String -> (Int, Int)
+toRowCol s = ((binSearch 0 127 (take 7 s)), (binSearch 0 7 (drop 7 s)))
+
+binSearch :: Int -> Int -> [Char] -> Int 
+binSearch _ hi [] = hi
+binSearch lo hi (s:ss) 
+                   | ss == [] && (s == 'F' || s == 'L')  = lo
+                   | ss == [] && (s == 'B' || s == 'R')   = hi
+                   | s == 'F' || s == 'L'  = binSearch lo ((hi+lo) `div` 2) ss
+                   | s == 'B' || s == 'R'  = binSearch ((hi+lo) `div` 2 + 1) hi ss
+                   | otherwise = 0
