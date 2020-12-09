7cdd6bd9fe6202e8cd9972c35ef8ced6f5c4b539
kakan <lucaskarlsson@live.se>
2020-12-09 01:52:22 +0100

    day8 complete

--- day8/part2.hs
+++ day8/part2.hs
@@ -6,12 +6,17 @@ main = do
     let parsedList = [(splitOn " " x !! 0, splitOn " " x !! 1) | x <- lines s]
     let ppl = [(x, p) | (x,y) <- parsedList, let p | head y == '-' = -1 * read (tail y) :: Int
                                                   | otherwise     =  1 * read (tail y) :: Int]
-    print $ findLoop ppl [] [] 0 0
+    print $ [findLoop (replaceElem ppl random "nop") [] [] 0 0 | random <- [0..600], (fst (ppl !! random)) == "jmp"]
+
+
+replaceElem :: [(String, Int)] -> Int -> String -> [(String, Int)]
+replaceElem operations index op = (take index operations) ++ [(op, (snd (operations !! index)))] ++ (drop (index+1) operations)
 
 findLoop :: [(String,Int)] -> [Int] -> [Int] -> Int -> Int -> Int
 findLoop operations alreadyDone oldVar index var 
-  | index `elem` alreadyDone = var
+  | index >= 632 = var
+-- | length operations == length alreadyDone
+  | index `elem` alreadyDone = 0
   | fst (operations !! index) == "acc" = findLoop operations (alreadyDone ++ [index]) (oldVar ++ [var]) (index + 1) (var + (snd (operations !! index)))
   | fst (operations !! index) == "jmp" = findLoop operations (alreadyDone ++ [index]) (oldVar ++ [var]) (index + (snd (operations !! index))) var
   | fst (operations !! index) == "nop" = findLoop operations (alreadyDone ++ [index]) (oldVar ++ [var]) (index + 1) var
-
