fdd32f3d07f379e10976273c17e7d7dd4e8feafe
kakan <lucaskarlsson@live.se>
2020-12-10 00:34:28 +0100

    optimized

--- day9/part2.hs
+++ day9/part2.hs
@@ -3,4 +3,4 @@ import Data.List
 main = do
     s <- readFile "input"
     let ls = [p | x <- lines s, let p = read x :: Int]
-    print $ [ minimum x + maximum x | x <-  filter (not . null) . concatMap inits . tails $ ls, sum x == 177777905]
+    print $ head [ minimum x + maximum x | x <-  filter (not . null) . concatMap inits . tails $ ls, sum x == 177777905]
