4ea93f0de322b36a4f969d2b0d9c409dcb643e5a
Lucas Karlsson <38735934+KarlssonLucas@users.noreply.github.com>
2021-12-07 17:16:56 +0100

    day7 done and optimized

--- aoc2021/day7/part1.hs
+++ aoc2021/day7/part1.hs
@@ -4,20 +4,5 @@ import Data.List
 main = do
     filecontent <- readFile "input"
     let a = (Prelude.map (read :: String -> Int) (splitOn "," filecontent))
-    print $ sumItUp (cheapestDistance a) [0..(maximum a)]
+    print $ sum $ map abs $ zipWith (-) (cycle [sort a !! (length a `div` 2)]) a
 
-cheapestDistance :: [Int] -> [(Int, Int)]
-cheapestDistance ls = [cheapestFromSub x ls n | x <- ls, n <- [0..(maximum ls)] ]
-
-cheapestFromSub :: Int ->  [Int] -> Int -> (Int, Int)
-cheapestFromSub x ls n = (abs (n-x), n)
-
-sumItUp :: [(Int, Int)] -> [Int] -> Int
-sumItUp [] ns = minimum $ zipMapList ns [0..100]
-sumItUp ((x, c):ls) ns = sumItUp ls (bbequals ns c (x + ns !! c))
-
-bbequals :: [Int] -> Int -> Int -> [Int]
-bbequals ls i n = take i ls ++ [n] ++ drop (i+1) ls
-
-zipMapList :: Num a => [a] -> [a] -> [a]
-zipMapList xs ys = zipWith (-) xs ys
---
+++ aoc2021/day7/part2.hs
@@ -0,0 +1,11 @@
+import Data.List.Split
+import Data.List
+
+main = do
+    filecontent <- readFile "input"
+    let a = (Prelude.map (read :: String -> Int) (splitOn "," filecontent))
+    print $ sum [distanceTo (minimum [average a - 1, average a + 1]) x | x <- a ] 
+        where average a = (sum a) `div` (length a)
+
+distanceTo :: Int -> Int -> Int 
+distanceTo h h1 = sum [1..(abs $ h-h1)]
